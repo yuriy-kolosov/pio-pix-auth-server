cdd9dbb76ad0b5ee94bd5ab831c098eec019626f
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2025-05-20 00:36:33 +0300

    pio-pix_authorization_server - начальный коммит

---
+++ src/main/java/pro/sky/pio_pix_auth_server/PioPixAuthServerApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.pio_pix_auth_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PioPixAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PioPixAuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/pio_pix_auth_server/PioPixAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.pio_pix_auth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PioPixAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
